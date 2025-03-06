28dfb45c0601704afa9d754158e77db3ae59f78e
svenchen-crypto <stevenchen@Stevens-MacBook-Air.local>
2025-03-05 20:04:31 -0500

    Sorting notebook modified

---
+++ Sliding Window/minSizeSubarraySum.py
@@ -0,0 +1,16 @@
+'''
+Given an array of positive integers nums and a positive integer target, return the minimal length of a 
+subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+
+Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+Output: 0
+'''
+
+def minSubArrayLen(target, nums):
+    pass
+
+res = minSubArrayLen(7, [2,3,1,2,4,3])
--- Sorting/quick_sort.py
+++ Sorting/quick_sort.py
@@ -1,21 +1,48 @@
 
 
-def hoare_partition(l, pivot):
-    pass
+def hoare_partition(l, lo, hi):
+    pivot = l[lo] 
+    i = lo
+    j = hi
+    print(f"Pivot is {pivot}")
+    while True:
+        while l[i] < pivot:
+            i+=1
+        while l[j] > pivot:
+            j-=1
+        if(i >= j):
+            return j
+        
+        print(f"Exchange {l[i]} with {l[j]}")
+        l[i], l[j] = l[j], l[i]
+    
 
 
+def lomuto_partition(l, lo, hi):
+    pivot = l[hi] 
+    i = lo - 1
+    print(f"Pivot is {pivot}")
+
+    for j in range(lo, hi):  
+        if l[j] <= pivot:
+            i += 1
+            print(f"Exchange {l[i]} with {l[j]}")
+            l[i], l[j] = l[j], l[i]  
+
+    print(f"Exchange {l[i+1]} with {l[hi]}")
+    l[i+1], l[hi] = l[hi], l[i+1]  
+
+    print('Partition done')
+    return i + 1 
+
+def quicksort(l, lo, hi):
+    if lo<hi:
+        pivot_idx = lomuto_partition(l, lo, hi)
+        quicksort(l, lo, pivot_idx-1)
+        quicksort(l, pivot_idx+1, hi)
 
 
-def quicksort(l):
-
-    pass
-
-
-
-def quick_select():
-    pass
-
-
-
-def randomized_quicksort(l):
-    pass
+l = [5, 2, 4, 6, 1, 3, 7, 6, 899, 20, 45, 31]
+print(f"Original list is {l}")
+quicksort(l, 0, len(l)-1)
+print(f"The sorted list is {l}")
